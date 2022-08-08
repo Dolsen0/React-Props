a3345d9925c90082ab07ad08ea06dc28da7f200f
dolsen0 <derek.olsen@gmail.com>
2022-08-08 13:53:07 -0400

    created initial react app with props

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return( 
+    <footer>
+        <p>&copy; 2022 Derek Olsen, LLC</p>
+    </footer>
+    )
+}
+
+
+
+export default Footer;
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Menubar(){
+    return (
+    <nav>
+        menubar
+    </nav>
+)
+
+}
+
+
+
+export default Menubar;
